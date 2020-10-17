fb36665fa475237bf698e7e0fd39749ffdb9900d
Mk-ism <mayankkumar2652@gmail.com>
2020-10-17 23:49:31 +0530

    delete added

--- server.js
+++ server.js
@@ -17,17 +17,82 @@ router.get("/",function(req,res){
 
 router.route("/users")
     .get(function(req,res){
+        ------------------------------------------------------
+    })
+    .post(function(req,res){
+        var db = new mongoOp();
         var response = {};
-        mongoOp.find({},function(err,data){
-        // Mongo command to fetch all data from collection.
+        // fetch email and password from REST request.
+        // Add strict validation when you use this in Production.
+        db.userEmail = req.body.email;
+        // Hash the password using SHA1 algorithm.
+        db.userPassword =  require('crypto')
+                          .createHash('sha1')
+                          .update(req.body.password)
+                          .digest('base64');
+        db.save(function(err){
+        // save() will run insert() command of MongoDB.
+        // it will add new data in collection.
             if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
+                response = {"error" : true,"message" : "Error adding data"};
             } else {
-                response = {"error" : false,"message" : data};
+                response = {"error" : false,"message" : "Data added"};
             }
             res.json(response);
         });
     });
+
+app.use('/',router);
+
+app.listen(3000);
+console.log("Listening to PORT 3000");
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var mongoOp     =   require("./models/mongo");
+var router      =   express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello World"});
+});
+
+router.route("/users")
+    .get(function(req,res){
+        ----------------------------------------
+    })
+    .post(function(req,res){
+        ----------------------------------------
+    });
+
+router.route("/users/:id")
+    .get(function(req,res){
+        ----------------------------------------
+    })
+    .put(function(req,res){
+        ----------------------------------------
+    })
+    .delete(function(req,res){
+        var response = {};
+        // find the data
+        mongoOp.findById(req.params.id,function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                // data exists, remove it.
+                mongoOp.remove({_id : req.params.id},function(err){
+                    if(err) {
+                        response = {"error" : true,"message" : "Error deleting data"};
+                    } else {
+                        response = {"error" : true,"message" : "Data associated with "+req.params.id+"is deleted"};
+                    }
+                    res.json(response);
+                });
+            }
+        });
+    })
 
 app.use('/',router);
 
